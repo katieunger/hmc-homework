eee23481a7a85a5314bf076a43bb4dbc0e5a29e9
Katie Unger <katie.e.unger@gmail.com>
2017-09-18 11:07:56 -0400

    adding notes on conditionals

--- Lesson1Notes.py
+++ Lesson1Notes.py
@@ -36,13 +36,13 @@ print "twitter"
 
 # Strings - The Basics
 
-# A string which contains any single quotes should be contained 
-# within double quotes.
+# # A string which contains any single quotes should be contained 
+# # within double quotes.
 print "My governor's name is Martin O'Malley"
 
-# This print causes a syntax error because the single quote
-# in O'Malley is interpreted by Python as the end of the string.
-# print My governor's name is Martin O'Malley'
+# # This print causes a syntax error because the single quote
+# # in O'Malley is interpreted by Python as the end of the string.
+# # print My governor's name is Martin O'Malley'
 
 # # \n starts a new line
 # # \t inserts a tab
@@ -88,7 +88,7 @@ print twitter[1:]
 # # variable.
 print phone1[1:4]
 
-# # Print the are code (including the parentheses) of phone1.
+# # Print the area code (including the parentheses) of phone1.
 print phone1[:6]
 
 # # Print the middle 3 numbers of phone1.
@@ -185,50 +185,104 @@ print article.count(" she said")
 # # Conditionals let you compare things and use the results of the comparisons
 # # to make decisions, or make your program behave differently
 
-# if gender == 'F':
-# 	print "Welcome to Hear Me Code"
+# # Conditionals are made of up "if" statements
+# # Equality is compared with a double equals sign
+if gender == 'F':
+	print "Welcome to Hear Me Code"
 
-# # Vars for Conditional Exercises
+# # Variables for Conditional Exercises
 students = 50
 capacity = 50
 teaching_assistants = 5
 
-# if students <= capacity:
-# 	print "Keep recruiting"
-# else:
-# 	print "End ticket signups"
+# # This simple conditional determines if you have reached or exceeded
+# # capacity for students.
+if students < capacity:
+	print "Keep recruiting"
+else:
+	print "End ticket signups"
 
+# # Elif is short for "else if" - defining another condition to test.
+# # This conditional determines if you have enough teaching assistants
+# # (there must be TA for every five students).
 
+if teaching_assistants == 0:
+	print "None? Uh oh!"
+elif teaching_assistants < students / 5:
+	print "Keep recruiting TAs"
+else:
+	print "Aren't the TAs great though?"
 
-# if students <= capacity:
-# 	print "Keep recruiting"
-# else:
-# 	print "End ticket signups"
+# # Operators are ways to compare two things.
+# # The greater than operator:
+print 5 > 5
+# # This evaluates as false because 5 is not greater than 5.
 
-# if teaching_assistants == 0:
-# 	print "None? Uh oh!"
-# elif teaching_assistants < students / 5:
-# 	print "Keep recruiting TAs"
-# else:
-# 	print "Aren't the TAs great though?"
-
-# print 5 > 5
-# print 5 >= 5
+# # The greater than or equal to operator:
+print 5 >= 5
+# # This evaluates as true because 5 is equal to 5.
 
 # # Conditional Exercise
-# volunteers = 90
-# goal = 100
-# if volunteers < goal:
-# 	volunteersNeeded = goal - volunteers
-# 	print "You are behind by {0} volunteers.".format(volunteersNeeded)
-# elif volunteers > goal:
-# 	extraVolunteers = goal - volunteers
-# 	print "You have surpassed your goal by {0} volunteers.".format(extraVolunteers)
-# elif volunteers == goal:
-# 	print "You have met your goal exactly!"
-# else:
-# 	print "Something ain't right."
+# # Variables
+volunteers = 90
+goal = 100
+
+# # This conditional logic will determine if you have met, surpassed,
+# # or fallen short of your goal in recruiting volunteers.
+if volunteers < goal:
+	volunteersNeeded = goal - volunteers
+	print "You are behind by {0} volunteers.".format(volunteersNeeded)
+elif volunteers > goal:
+	extraVolunteers = goal - volunteers
+	print "You have surpassed your goal by {0} volunteers.".format(extraVolunteers)
+elif volunteers == goal:
+	print "You have met your goal exactly!"
+else:
+	print "Something ain't right."
 
 # # Complex Conditionals
+# # Using string methods within our conditional statement, we can test
+# # if gender is equal to F regardless of capitalization:
 if gender.lower() == "f":
 	print "Welcome to Hear Me Code"
+
+## Compound Conditionals
+
+# # We can join conditionals using "and" or "or" keywords.
+# # Using "or", either condition must be true.
+# # This tests if the email is valid - it is not valid if it contains 0
+# # or more than 1 "@".
+if email.count("@") > 1 or email.count("@") == 0:
+	print "Not a valid email address"
+else:
+	print "The email address is valid"
+
+men_quoted = article.count(" he said")
+women_quoted = article.count(" she said")
+
+# # Using "and", both conditions must be true.
+# # This tests if the number of times men were quoted in the article
+# # and the number of times women were quoted both equal 0.
+if men_quoted == 0 and women_quoted == 0:
+	print "Neither men nor women were quoted"
+
+# # This tests if the number of times women were quoted was 0,
+# # or if men were quoted exactly twice as many times as women.
+if women_quoted == 0 or men_quoted == women_quoted * 2:
+	print """No women were quoted, or twice as many quotes came from men"""
+
+# # Nested Conditionals
+# # Conditonals can be nested inside each other to define more logic to check
+# # if the first set of conditions are met.
+# # If the article contains more than 0 quotes, test if men were quoted more,
+# # less, or equally.
+if men_quoted == 0 and women_quoted == 0:
+	print "Neither men nor women were quoted"
+else: 
+	if men_quoted > women_quoted:
+		print "More men than women were quoted"
+	elif women_quoted > men_quoted:
+		print "More women than men were quoted"
+	else:
+		print "Men and women were quoted equally"
+
