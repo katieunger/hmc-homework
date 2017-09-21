f5ade2159ee0ebc0aee83c1d4297316f7edaebb3
Katie Unger <katie.e.unger@gmail.com>
2017-09-21 16:49:51 -0400

    updating code for goal 3 and adding more comments

--- pbj.py
+++ pbj.py
@@ -1,21 +1,23 @@
 # Variables
-bread = 2
-peanutButter = 2
-jelly = 1
+bread = 3
+peanutButter = 7
+jelly = 0
 sandwiches = bread/2
+minIngredientQuantity = min(bread, peanutButter, jelly)
 
 # Goal 1
 # Create a program that can tell you whether or not you can make a peanut butter and jelly sandwich
 print "Goal 1"
+print "Can I make any peanut butter and jelly sandwiches?"
 if bread > 1 and peanutButter >= 1 and jelly >= 1:
-	print "You can make at least one peanut butter and jelly sandwich."
+	print "Yes, you can make at least one peanut butter and jelly sandwich."
 else:
-	print "You don't have sufficient ingredients to make any peanut butter and jelly sandwiches."
+	print "No, you don't have sufficient ingredients to make any peanut butter and jelly sandwiches."
 
 # Goal 2
 # Create a program to tell you: if you can make a sandwich, how many you can make
 print "Goal 2"
-minIngredientQuantity = min(bread, peanutButter, jelly)
+print "How many peanut butter and jelly sandwiches can I make?"
 if bread > 1 and peanutButter >= 1 and jelly >= 1:
 	print "You can make {0} sandwich(es).".format(minIngredientQuantity)
 else:
@@ -24,35 +26,47 @@ else:
 # Goal 3
 # Create a program to allow you to make open-face sandwiches if you have an odd number of slices of bread
 print "Goal 3"
-if sandwiches > 0 and peanutButter >= 1 and jelly >= 1:
-	print "You can make {0} sandwich(es).".format(sandwiches)
+print "How many peanut butter and jelly sandwiches can I make if I can also make open-face sandwiches?"
+if bread > 1 and peanutButter >= 1 and jelly >= 1:
+	print "You can make {0} sandwich(es).".format(minIngredientQuantity)
 	if bread%2 != 0:
 		if peanutButter%2 != 0 and jelly%2 != 0:
 			print "You have an odd number of bread slices, and an odd amount of peanut butter and jelly. You can also make an open-face sandwich."
 		else:
 			print "You have an odd number of bread slices, but not an odd amount of peanut butter and jelly. You can't make an additional open-face sandwich."
 	else:
-		print "You have an even number of bread slices."
+		print "You have an even number of bread slices, so you don't have any leftover bread to make open-face sandwiches."
 else:
 	print "You don't have sufficient ingredients to make any peanut butter and jelly sandwiches."
 
 # Goal 4
+# Create a program to tell you: if you're missing ingredients, which ones you need to be able to make your sandwiches
 print "Goal 4"
-if sandwiches > 0 and peanutButter >= 1 and jelly >= 1:
-	print "You can make {0} peanut butter and jelly sandwich(es).".format(sandwiches)
+print ""
+if bread > 1 and peanutButter >= 1 and jelly >= 1:
+	print "You can make {0} peanut butter and jelly sandwich(es).".format(minIngredientQuantity)
 	if bread%2 != 0:
 		if peanutButter%2 != 0 and jelly%2 != 0:
 			print "You have an odd number of bread slices, and an odd amount of peanut butter and jelly. You can also make an open-face sandwich."
 		else:
 			print "You have an odd number of bread slices, but not an odd amount of peanut butter and jelly. You can't make an additional open-face sandwich."
 	else:
-		print "You have an even number of bread slices."
-elif sandwiches > 0 and peanutButter >= 1 and jelly < 1:
-	print "You can make {0} peanut butter sandwich(es).".format(sandwiches)
+		print "You have an even number of bread slices, so you don't have any leftover bread to make open-face sandwiches."
+elif bread > 1 and peanutButter >= 1 and jelly < 1:
+	print "You can make {0} peanut butter sandwich(es).".format(minIngredientQuantity)
 else:
 	print "You don't have sufficient ingredients to make any sandwiches."
+	if bread == 0:
+		print "You have no bread!"
+	elif peanutButter == 0:
+		print "You have no peanut butter!"
+	elif jelly == 0:
+		print "You have no jelly!"
+	else:
+		print "Error"
 
 # Goal 5
+# Create a program to tell you: if you have enough bread and peanut butter but no jelly, that you can make a peanut butter sandwich
 print "Goal 5"
 if sandwiches > 0 and peanutButter >= 1 and jelly >= 1:
 	print "You can make {0} peanut butter and jelly sandwich(es).".format(sandwiches)
@@ -62,7 +76,7 @@ if sandwiches > 0 and peanutButter >= 1 and jelly >= 1:
 		else:
 			print "You have an odd number of bread slices, but not an odd amount of peanut butter and jelly. You can't make an additional open-face sandwich."
 	else:
-		print "You have an even number of bread slices."
+		print "You have an even number of bread slices, so you don't have any leftover bread to make open-face sandwiches."
 elif sandwiches > 0 and peanutButter >= 1 and jelly < 1:
 	print "You can make {0} peanut butter sandwich(es).".format(sandwiches)
 else:
