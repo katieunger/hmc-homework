4b0aca236dd4971c2e022054873f57253a6b0cda
Katie Unger <katie.e.unger@gmail.com>
2017-09-24 23:56:02 -0400

    updating PB&J exercise - fixing code for goal 3 and adding comments

--- lesson_1/pbj.py
+++ lesson_1/pbj.py
@@ -1,14 +1,15 @@
 # Variables
-bread = 3
-peanutButter = 7
-jelly = 1
+bread = 7
+peanutButter = 5
+jelly = 2
 sandwiches = bread/2
-minIngredientQuantity = min(bread, peanutButter, jelly)
+minIngredientQuantity = min(sandwiches, peanutButter, jelly)
 
 # Goal 1
 # Create a program that can tell you whether or not you can make a peanut butter and jelly sandwich
 print "Goal 1"
 print "Can I make any peanut butter and jelly sandwiches?"
+# If I have more than one bread slice, at least one serving of peanut butter, and at least one serving of jelly, I can make a PB&J sandwich.
 if bread > 1 and peanutButter >= 1 and jelly >= 1:
 	print "Yes, you can make at least one peanut butter and jelly sandwich."
 else:
@@ -18,6 +19,7 @@ else:
 # Create a program to tell you: if you can make a sandwich, how many you can make
 print "Goal 2"
 print "How many peanut butter and jelly sandwiches can I make?"
+# If I have more than one bread slice, at least one serving of peanut butter, and at least one serving of jelly, I can make as many PB&J sandwiches as the the number of servings of the ingredient I have the least of.
 if bread > 1 and peanutButter >= 1 and jelly >= 1:
 	print "You can make {0} sandwich(es).".format(minIngredientQuantity)
 else:
@@ -27,13 +29,17 @@ else:
 # Create a program to allow you to make open-face sandwiches if you have an odd number of slices of bread
 print "Goal 3"
 print "How many peanut butter and jelly sandwiches can I make if I can also make open-face sandwiches?"
+# If I have more than one bread slice, at least one serving of peanut butter, and at least one serving of jelly, I can make as many PB&J sandwiches as the the number of servings of the ingredient I have the least of.
 if bread > 1 and peanutButter >= 1 and jelly >= 1:
-	print "You can make {0} sandwich(es).".format(minIngredientQuantity)
+	print "You can make {0} sandwich(es) with two slices of bread.".format(minIngredientQuantity)
+	# If I have an odd amount of bread, I may be able to make an open-face sandwich - there is a leftover slice of bread.
 	if bread%2 != 0:
-		if peanutButter%2 != 0 and jelly%2 != 0:
-			print "You have an odd number of bread slices, and an odd amount of peanut butter and jelly. You can also make an open-face sandwich."
+		# Do I have enough leftover PB&J after the sandwiches with two bread slices are made to make an open face sandwich?
+		if peanutButter - minIngredientQuantity >= 1 and jelly - minIngredientQuantity >= 1:
+			print "You can make an additional open-face sandwich."
+			# print "You have an odd number of bread slices, and an odd amount of peanut butter and jelly. You can also make an open-face sandwich."
 		else:
-			print "You have an odd number of bread slices, but not an odd amount of peanut butter and jelly. You can't make an additional open-face sandwich."
+			print "You have leftover bread, but you've used up all your peanut buter and jelly. You cannot make an additional open-face sandwich."
 	else:
 		print "You have an even number of bread slices, so you don't have any leftover bread to make open-face sandwiches."
 else:
