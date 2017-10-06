edeaf0f8be20ba1b3a54410391e8c33e0c6d9f32
Katie Unger <katie.e.unger@gmail.com>
2017-10-06 16:55:04 -0400

    updating pb&j loops to meet goal 2 - print how much of each ingredient is left while looping

--- lesson_2/lesson_2_pbj_while.py
+++ lesson_2/lesson_2_pbj_while.py
@@ -33,13 +33,13 @@
 # Goal #1: Write a new version of the PB&J program that uses a while loop.  Print "Making sandwich #" and the number of the sandwich until you are out of bread, peanut butter, or jelly.
 
 # Variables
-bread = 5
-peanutButter = 3
-jelly = 10
+bread = 8
+peanutButter = 10
+jelly = 3
 sandwiches = bread/2
 minIngredientQuantity = min(sandwiches, peanutButter, jelly)
-ingredientsArray = [bread, peanutButter, jelly]
 
+print("Goal 1")
 for number in range(minIngredientQuantity):
 	print("Making sandwich #{0}".format(number+1))
 	bread = bread-2
@@ -54,3 +54,29 @@ for number in range(minIngredientQuantity):
 		print("All done; only had enough jelly for {0} sandwiches.".format(number+1))
 	else:
 		print("I can make another sandwich...")
+
+# Goal #2: Modify that program to say how many sandwiches-worth of each ingredient remains.
+
+# Variables
+bread = 10
+peanutButter = 10
+jelly = 4
+sandwiches = bread/2
+minIngredientQuantity = min(sandwiches, peanutButter, jelly)
+
+print("\nGoal 2")
+for number in range(minIngredientQuantity):
+	bread = bread-2
+	peanutButter -= 1
+	jelly = jelly-1
+	sandwiches = bread/2
+	print("Making sandwich #{0}".format(number+1))
+	if sandwiches == 0:
+		print("All done; only had enough bread for {0} sandwiches.".format(number+1))
+	elif peanutButter == 0:
+		print("All done; only had enough peanut butter for {0} sandwiches.".format(number+1))
+	elif jelly == 0:
+		print("All done; only had enough jelly for {0} sandwiches.".format(number+1))
+	else:
+		print("I have enough bread for {0} more sandwiches, enough peanut butter for {1} more, and enough jelly for {2} more.".format(sandwiches, peanutButter, jelly))
+
