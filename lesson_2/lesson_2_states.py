1c98eb145c45bb67853f02a41777cc5e989c3a66
Katie Unger <katie.e.unger@gmail.com>
2017-10-11 15:45:30 -0400

    updating lesson 2 states exercise to print select HTML and saving output

--- lesson_2/lesson_2_states.py
+++ lesson_2/lesson_2_states.py
@@ -30,5 +30,8 @@
 
 stateList = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District Of Columbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 
+print("<select>")
 for state in stateList:
-	print state
+	print("<option value='stateAbbreviation'>{0}</option>".format(state))
+print("</select>")
+
