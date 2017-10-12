334057cbe00fa5401817c5b4071b8587c1a325d5
Katie Unger <katie.e.unger@gmail.com>
2017-10-12 16:24:35 -0400

    working on fibonacci exercise

---
+++ lesson_2/lesson_2_fibonacci.py
@@ -0,0 +1,27 @@
+# Fibonnaci Sequence
+
+# Difficulty: Advanced
+
+# NOTE: This is *not* a practical example of how you'll use coding in your day to day, BUT it does appear on a surprising number of in-person coding interviews.  Why employers rely on something so impractical to gauge your coding ability is beyond me.
+#       But since they're using it, maybe in this narrow sense it's a practical exercise to know how to do after all.
+
+
+# The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... The next number is found by adding up the two numbers before it.
+
+# Create a program that will generate the first 10 numbers in the Fibonacci Sequence.
+
+# When completed, your program should have the output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+numberList = []
+for number in range(10):
+	numberList.append(number)
+print("Number List:{0}".format(numberList))
+
+FibonacciList = []
+for index, number in enumerate(numberList):
+	if(number==0):
+		previousNumber = 0
+	else:
+		previousNumber = numberList[index-1]
+	print("{0}+{1}".format(previousNumber,number))
+	FibonacciList.append(previousNumber+number)
+print FibonacciList
